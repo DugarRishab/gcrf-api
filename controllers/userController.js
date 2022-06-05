56c113a37483ff29f892ffbb579db2a4464b35ae
Rishab Dugar <www.anitadugar9163@gmail.com>
2022-06-05 22:46:27 +0530

    updated leaderboard function for better privacy

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,12 +4,22 @@ const User = require("./../userModel");
 
 exports.getLeaderboard = catchAsync(async (req, res, next) => {
 	const users = await User.find();
+
+	const milestone1 = users.filter(user => user.milestones[0] >= 100).map(user => user.name);
+	const milestone2 = users.filter(user => user.milestones[1] >= 100).map(user => user.name);
+	const milestone3 = users.filter(user => user.milestones[2] >= 100).map(user => user.name);
+	const milestone4 = users.filter(user => user.milestones[3] >= 100).map(user => user.name);
+	
 	res.status(200).json({
 		message: "success",
 		data: {
-			users
+			milestone1,
+			milestone2,
+			milestone3,
+			milestone4
 		}
 	});
+	
 })
 exports.getReport = catchAsync(async (req, res, next) => {
 	
@@ -37,5 +47,5 @@ exports.newReport = catchAsync(async (req, res, next) => {
 	});
 	res.status(200).json({
 		message: 'success'
-    });
+	});
 });
